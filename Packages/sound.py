71010877f11b30a6fc7eced7ae8d0919439d718e
Mochizuki Ken <140779472+Mochizuki-Ken@users.noreply.github.com>
2024-05-03 10:55:03 +0800

    Update sound.py

--- Packages/sound.py
+++ Packages/sound.py
@@ -1,1 +1,33 @@
-print()
+from playsound import playsound
+import time
+
+class Sound():
+    
+    SOUNDS_PATH = {
+    	"Noteflight-1":"/path",
+        "Done":"/path"
+    }
+    
+    def __init__(self) -> None:
+        
+        return 
+        
+    def DoneSound(self):
+        
+        playsound( self.SOUNDS_PATH["Done"] )
+        
+        return 
+        
+    def Play(self,Type,Rate,LoopType):
+        
+        if( Type not in self.SOUNDS_PATH.keys() ):
+            
+            return "error-No Type"
+                
+        for i in LoopType:
+            
+            playsound(self.SOUNDS_PATH[Type])
+            
+            time.wait(rate)
+        
+        return
