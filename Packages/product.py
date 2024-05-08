8349ec8a4aafc95c6f07f758eb9072353f26b134
Mochizuki Ken <140779472+Mochizuki-Ken@users.noreply.github.com>
2024-05-08 16:29:35 +0800

    Update product.py

---
+++ Packages/product.py
@@ -0,0 +1,15 @@
+from .speak import Text_to_Voice
+from .sound import Sound
+from .Data import *
+
+class Product():
+    
+    
+    
+    def __init__(self) -> None:
+        
+        pass
+        
+    def ChooseTargetProduct(self,CurrentTargetProducts):
+        
+        return False
