c2c0268276e2b180070884c19999f1a73d64d7f3
Mochizuki Ken <140779472+Mochizuki-Ken@users.noreply.github.com>
2024-05-08 09:05:49 +0800

    Update Data.py

--- Packages/Data.py
+++ Packages/Data.py
@@ -1,1 +1,26 @@
-RECOMAND_LIST = {}
+PRODUCT_LIST = [
+	{
+		"NAME":"咖啡",
+        "PRICE":12
+    },
+    {
+    	"NAME":"牙膏",
+        "PRICE":22
+    },
+    {
+    	"NAME":"牙膏2",
+        "PRICE":20
+    }
+]
+
+RECOMAND_LIST = {
+    "牙膏":"牙膏2"
+}
+
+PRODUCT_DISCOUNT_LIST = {
+    "牙膏2":"宜家做緊買一送一優惠"
+}
+
+EVENT_DISCOUNT_LIST = [
+    "全場生果九五折！買滿200蚊即送富士蘋果一個！"
+]
